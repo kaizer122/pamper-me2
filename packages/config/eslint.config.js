8114933d3ae2f0cb1574f35ace06dba7f4350bdf
kais ladjemi <kais.ladjemi@medius.com>
2026-05-02 11:31:24 +0100

    fix(config): add type:module, remove unnecessary spread in eslint config

--- packages/config/eslint.config.js
+++ packages/config/eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   {
     rules: {
       "@typescript-eslint/no-unused-vars": [
